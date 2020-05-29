3a4ce8ba9c4ddd2e3007b1a57a5fb3b9c0d5c488
shivam14300 <shivam.bansal@students.iiit.ac.in>
2020-05-29 16:19:03 +0530

    dev branch

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,43 +2,43 @@
 
 import React from 'react';
 import App from '../App';
-import {shallow, mount} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import '../setupTests';
 import renderer from 'react-test-renderer';
 
-describe ('Snapshot Testing', () => {
-  it ('test snapshot on initial rendering', () => {
-    const component = renderer.create (<App />);
+describe('Snapshot Testing', () => {
+  it('test snapshot on initial rendering', () => {
+    const component = renderer.create(<App />);
     // console.log (component);
     // console.log (component.toJSON ());
-    let tree = component.toJSON ();
-    expect (tree).toMatchSnapshot ();
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
 
     // const component = shallow (<App />);
     // expect (component).toMatchSnapshot;
   });
 
-  it (
+  it(
     'test snapshot after API_GET',
     () => {
-      expect.assertions (1);
-      const component = renderer.create (<App />);
-      return component.getInstance ().changeTitle1 ().then (() => {
-        let tree = component.toJSON ();
-        expect (tree).toMatchSnapshot ();
+      expect.assertions(1);
+      const component = renderer.create(<App />);
+      return component.getInstance().changeTitle1().then(() => {
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
       });
     },
     1000000
   );
 
-  it (
+  it(
     'test snapshot after API_GET1',
     () => {
-      expect.assertions (1);
-      const component = renderer.create (<App />);
-      return component.getInstance ().API_GET1 ().then (() => {
-        let tree = component.toJSON ();
-        expect (tree).toMatchSnapshot ();
+      expect.assertions(1);
+      const component = renderer.create(<App />);
+      return component.getInstance().API_GET1().then(() => {
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
       });
     },
     100000
@@ -58,52 +58,52 @@ describe ('Snapshot Testing', () => {
   // );
 });
 
-describe ('Mock Testing', () => {
-  it ('Mocking axios get function', () => {
-    const res = new App ();
-    res.loadTitle ().then (title => {
-      expect (title).toEqual ('DELECTUS AUT AUTEM');
+describe('Mock Testing', () => {
+  it('Mocking axios get function', () => {
+    const res = new App();
+    res.loadTitle().then(title => {
+      expect(title).toEqual('DELECTUS AUT AUTEM');
     });
   });
 });
 
-describe ('Testing Button Clicks', () => {
+describe('Testing Button Clicks', () => {
   let wrapper;
-  it ('Testing Button Click of API1', () => {
-    wrapper = shallow (<App />);
-    wrapper.find ('#button1').simulate ('click');
-    wrapper.update ();
-    expect (wrapper.state ('title1')).toEqual ('Calling API1...');
+  it('Testing Button Click of API1', () => {
+    wrapper = shallow(<App />);
+    wrapper.find('#button1').simulate('click');
+    wrapper.update();
+    expect(wrapper.state('title1')).toEqual('Calling API1...');
   });
-  it ('Testing Button Click of API2', () => {
-    wrapper = shallow (<App />);
-    wrapper.find ('#button2').simulate ('click');
-    wrapper.update ();
-    expect (wrapper.state ('title2')).toEqual ('Calling API2...(Please Wait)');
+  it('Testing Button Click of API2', () => {
+    wrapper = shallow(<App />);
+    wrapper.find('#button2').simulate('click');
+    wrapper.update();
+    expect(wrapper.state('title2')).toEqual('Calling API2...(Please Wait)');
   });
-  it ('Testing Button Click of API3', () => {
-    wrapper = shallow (<App />);
-    wrapper.find ('#button3').simulate ('click');
-    wrapper.update ();
-    expect (wrapper.state ('title3')).toEqual ('Calling API3...');
+  it('Testing Button Click of API3', () => {
+    wrapper = shallow(<App />);
+    wrapper.find('#button3').simulate('click');
+    wrapper.update();
+    expect(wrapper.state('title3')).toEqual('Calling API3...');
   });
 });
 
-describe ('Testing API function', () => {
-  const fake = jest.spyOn (App.prototype, 'API_GET');
-  const wrapper = shallow (<App />);
-  wrapper.find ('#button1').simulate ('click');
-  wrapper.update ();
-  it ('check that function API_GET is defined', () => {
-    expect (fake).toBeDefined (); // check that function API_GET is defined
+describe('Testing API function', () => {
+  const fake = jest.spyOn(App.prototype, 'API_GET');
+  const wrapper = shallow(<App />);
+  wrapper.find('#button1').simulate('click');
+  wrapper.update();
+  it('check that function API_GET is defined', () => {
+    expect(fake).toBeDefined(); // check that function API_GET is defined
   });
 
-  it ('check that function API_GET is called on click', () => {
-    expect (fake).toHaveBeenCalled (); // check that function API_GET is called
+  it('check that function API_GET is called on click', () => {
+    expect(fake).toHaveBeenCalled(); // check that function API_GET is called
   });
 
-  it ('check that function API_GET returns successfully on calling', () => {
-    expect (fake).toHaveReturned (); // check that function API_GET returns successfully
+  it('check that function API_GET returns successfully on calling', () => {
+    expect(fake).toHaveReturned(); // check that function API_GET returns successfully
   });
 });
 
